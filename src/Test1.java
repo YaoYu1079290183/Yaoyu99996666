9e126b0ee0a7409f6826170eee41e7f12bd4d75c
YaoYu1079290183 <yaoyu745464872@163.com>
2019-01-06 11:50:56 +0800

    test13333dddd

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
     public static void main(String args[]){
 
         System.out.print("Hello Git,I'm your father!!!!!");
-        System.out.print("sadsssasdasdsa");
+        System.out.print("saasdasdsa");
     }
 
 }
